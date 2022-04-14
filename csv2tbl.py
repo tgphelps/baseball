7011c99a4356f1fe6cc77260fbe15b3bcb127e8d
Terry Phelps <tgphelps50@gmail.com>
2022-04-14 13:02:33 -0400

    save

--- csv2tbl.py
+++ csv2tbl.py
@@ -4,7 +4,7 @@ csv2tbl.py: Deduplicating utility for backup/restore
 
 Usage:
     csv2tbl.py [--debug] create DEFNFILE
-    csv2tbl.py [--debug] [-H] load DEFNFILE CSFILE
+    csv2tbl.py [--debug] [-H] load DEFNFILE CSVFILE
     csv2tbl.py --version
     csv2tbl.py --help
 
@@ -24,16 +24,32 @@ VERSION = 0.01
 
 class Globals:
     debug: bool
+    has_header: bool
 
 
 g = Globals()
 
 
-def main():
+def main() -> None:
     args = docopt.docopt(__doc__, version=VERSION)
     g.debug = args['--debug']
+    g.has_header = args['-H']
     if g.debug:
         print(args)
+    if args['create']:
+        create_table(args['DEFNFILE'])
+    elif args['load']:
+        load_table(args['DEFNFILE'], args['CSVFILE'])
+    else:
+        assert False
+
+
+def create_table(defn_file: str) -> None:
+    print('create table:', defn_file)
+
+
+def load_table(defn_file: str, csv_file: str) -> None:
+    print('load table:', defn_file, csv_file)
 
 
 if __name__ == '__main__':
