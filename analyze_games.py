a708db0e2736a0a0fb632c135b18ed971a8c2e9f
Terry Phelps <tgphelps50@gmail.com>
2022-06-25 16:18:31 -0400

    save

--- analyze_games.py
+++ analyze_games.py
@@ -11,8 +11,9 @@ Options:
     --version          Show version.
 
 <analysis> can be one of:
-    count-games         Count games in database
-    innings-by-runs     Show how often N runs occur in an inning
+    count-games         Count games in database.
+    innings-by-runs     Show how often N runs occur in an inning.
+    home-advantage      Show percentage of games home team won.
 """
 
 from collections import defaultdict
@@ -25,12 +26,12 @@ import util
 VERSION = "0.0.1"
 
 
-class Globals():
-    runs: defaultdict[int, int]
+# class Globals():
+#    runs: defaultdict[int, int]
 
 
-g = Globals()
-g.runs = defaultdict(lambda: 0)
+# g = Globals()
+# g.runs = defaultdict(lambda: 0)
 
 
 def main():
@@ -46,28 +47,49 @@ def main():
                 count_games(cur)
             elif s == 'innings-by-runs':
                 innings_by_runs(cur)
+            elif s == 'home-advantage':
+                home_advantage(cur)
             else:
                 print('ERROR: bad analysis specified')
 
 
 def count_games(cur: psycopg2.extensions.cursor) -> None:
+    "Show total number of games in the database."
     cur.execute('select count(*) from gamelogs')
     n = cur.fetchone()[0]
     print(f'Total games played: {n}')
 
 
 def innings_by_runs(cur: psycopg2.extensions.cursor) -> None:
+    "Show distribution of runs scored in an inning."
+    runs: defaultdict[int, int] = defaultdict(lambda: 0)
     cur.execute("select v_line_score, h_line_score from gamelogs")
     for row in cur:
         for i in util.line_score_to_ints(row[0]):
-            g.runs[i] += 1
+            runs[i] += 1
         for i in util.line_score_to_ints(row[1]):
-            g.runs[i] += 1
-    total = sum([g.runs[i] for i in g.runs])
-    print('Total:', total)
-    for r in sorted(g.runs.keys()):
-        pct = float(g.runs[r]) / total * 100
-        print(r, '->', g.runs[r], f'{pct:4.2f}%')
+            runs[i] += 1
+    total = sum([runs[i] for i in runs])
+    print('Total innings:', total)
+    for r in sorted(runs.keys()):
+        pct = float(runs[r]) / total * 100.0
+        print(r, '->', runs[r], f'{pct:4.2f}%')
+
+
+def home_advantage(cur: psycopg2.extensions.cursor) -> None:
+    "Show percentage of games won by the home team."
+    games = 0
+    visitor_wins = 0
+    home_wins = 0
+    cur.execute("select v_score, h_score from gamelogs")
+    for row in cur:
+        games += 1
+        if row[0] > row[1]:
+            visitor_wins += 1
+        else:
+            home_wins += 1
+    pct = float(home_wins) / games * 100.0
+    print(f'Home winning percentage: {pct:4.2f}')
 
 
 if __name__ == '__main__':
